b71bc9c769746c75c6ab6135da362c8cda6718ad
Savva Ponomarev <s.ponomarev@innopolis.university>
2025-11-16 21:50:24 +0300

    favorite screen

--- idea_generator/main.dart
+++ idea_generator/main.dart
@@ -163,3 +163,48 @@ class ActivityCard extends StatelessWidget {
     }
   }
 }
+
+class FavoritesScreen extends StatelessWidget {
+  const FavoritesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ActivityProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("My Favorites")),
+      body: provider.favorites.isEmpty
+          ? const Center(
+              child: Text(
+                "No favorites yet.\nGo swipe some ideas!",
+                textAlign: TextAlign.center,
+              ),
+            )
+          : ListView.builder(
+              itemCount: provider.favorites.length,
+              itemBuilder: (context, index) {
+                final item = provider.favorites[index];
+                return Dismissible(
+                  key: Key(item.key),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (_) => provider.removeFavorite(item),
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerRight,
+                    padding: const EdgeInsets.only(right: 20),
+                    child: const Icon(Icons.delete, color: Colors.white),
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.deepPurple[100],
+                      child: Icon(Icons.star, color: Colors.deepPurple),
+                    ),
+                    title: Text(item.activity),
+                    subtitle: Text(item.type),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+}
